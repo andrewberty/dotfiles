35b5ac79f0d8a04750d4d5982068474145d2ec2c
Andrew Berty <andrewgeorgeberty@gmail.com>
2024-08-03 20:10:01 +0300

    Update lualine config

--- nvim/lua/plugins/configs/lualine.lua
+++ nvim/lua/plugins/configs/lualine.lua
@@ -1,65 +1,56 @@
+local theme = require("lualine.themes.auto")
+
+local position = "bottom"
 local sections = {
 	lualine_a = { "branch" },
 	lualine_b = {
-		{
-			"buffers",
-			symbols = { alternate_file = "" },
-		},
+		{ "buffers", symbols = { alternate_file = "" } },
 		{ "diagnostics", sources = { "nvim_lsp" }, symbols = { error = " ", warn = " ", info = " " } },
 	},
 	lualine_c = {},
 	lualine_x = {},
 	lualine_y = {},
-	lualine_z = {
-		{
-			"filename",
-			path = 3,
-		},
-	},
+	lualine_z = { { "filename", path = 1, file_status = false, shorting_target = 60 } },
 }
 
-local template = {
-	a = "Title",
-	b = "Normal",
-	c = "Normal",
-}
+local custom_theme = function()
+	local t = {}
+	for key, _ in pairs(theme) do
+		if key == "inactive" then
+			t[key] = { a = "NonText", b = "NonText", c = "NonText" }
+		else
+			t[key] = { a = "Title", b = "Normal", c = "Normal", z = "Title" }
+		end
+	end
+	return t
+end
 
-local custom_theme = {
-	normal = template,
-	insert = template,
-	visual = template,
-	replace = template,
-	command = template,
-	inactive = {
-		a = "NonText",
-		b = "NonText",
-		c = "NonText",
-	},
-}
-local ignored = {
-	"help",
-	"NvimTree",
-	"neo-tree",
-	"alpha",
-	"lazy",
-	"starter",
-}
+local ignored = { "help", "NvimTree", "lazy", "starter" }
 
-require("lualine").setup({
+local options = {
 	options = {
-		theme = custom_theme,
+		theme = custom_theme(),
 		icons_enabled = true,
 		section_separators = "",
 		component_separators = "",
-		disabled_filetypes = {
-			statusline = ignored,
-			winbar = ignored,
-		},
+		disabled_filetypes = { statusline = ignored, winbar = ignored },
 	},
-	winbar = {},
-	inactive_winbar = {},
-	-- sections = {},
-	-- inactive_sections = {},
-	sections = sections,
-	inactive_sections = sections,
-})
+}
+
+if position == "top" then
+	require("lualine").setup(
+		vim.tbl_deep_extend(
+			"force",
+			options,
+			{ winbar = sections, inactive_winbar = sections, sections = {}, inactive_sections = {} }
+		)
+	)
+elseif position == "bottom" then
+	require("lualine").setup(
+		vim.tbl_deep_extend(
+			"force",
+			options,
+			{ winbar = {}, inactive_winbar = {}, sections = sections, inactive_sections = sections }
+		)
+	)
+end
