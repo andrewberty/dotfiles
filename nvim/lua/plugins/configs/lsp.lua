faafaddd4366ac5148f8fac17381b50fcaf2a3c9
Andrew Berty <andrewgeorgeberty@gmail.com>
2024-08-23 14:35:49 +0300

    disable prettier formatting for markdown

--- nvim/lua/plugins/configs/lsp.lua
+++ nvim/lua/plugins/configs/lsp.lua
@@ -116,6 +116,9 @@ conform.setup({
 	format_on_save = function(bufnr)
 		-- Disable with a global or buffer-local variable
 		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
+
+		local ignore_filetypes = { "markdown" }
+		if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then return end
 		return { timeout_ms = 500, lsp_fallback = true }
 	end,
 })
