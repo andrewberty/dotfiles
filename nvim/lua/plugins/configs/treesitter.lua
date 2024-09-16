7adea93c17e154d998a1d7e2e8ffb9b3ceb6fbcc
Andrew Berty <andrewgeorgeberty@gmail.com>
2024-09-16 16:45:06 +0300

    php laravel blade neovim syntax highlighting

--- nvim/lua/plugins/configs/treesitter.lua
+++ nvim/lua/plugins/configs/treesitter.lua
@@ -45,3 +45,20 @@ require("nvim-treesitter.configs").setup({
 		},
 	},
 })
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+parser_config.blade = {
+	install_info = {
+		url = "https://github.com/EmranMR/tree-sitter-blade",
+		files = { "src/parser.c" },
+		branch = "main",
+	},
+	filetype = "blade",
+}
+
+vim.filetype.add({
+	pattern = {
+		[".*%.blade%.php"] = "blade",
+	},
+})
