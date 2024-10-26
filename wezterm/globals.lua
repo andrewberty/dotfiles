24e4df8887a6a3505d2c396a3e8c8dcc5c877ec4
Andrew Berty <andrewgeorgeberty@gmail.com>
2024-10-26 18:02:57 +0300

    include globals file

---
+++ wezterm/globals.lua
@@ -0,0 +1,12 @@
+return {
+  opacity = 1,
+  colorscheme = "catppuccin-mocha",
+  padding = {
+    bottom = 20,
+    top = 20,
+    left = 20,
+    right = 20,
+  },
+  OLED = false,
+  font = "Rec Mono Linear",
+}
