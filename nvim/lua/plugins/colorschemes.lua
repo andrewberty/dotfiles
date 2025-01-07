a9121d539abc366add967f082e2a714de9a50a4b
Andrew Berty <andrewgeorgeberty@gmail.com>
2025-01-07 13:48:48 +0200

    nvim: colorschemes cleanup

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -21,56 +21,6 @@ return {
 		end,
 	},
 	{
-		"sainnhe/gruvbox-material",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			for key, value in pairs({
-				gruvbox_material_foreground = "material",
-				gruvbox_material_diagnostic_virtual_text = "colored",
-				gruvbox_material_ui_contrast = "high",
-				gruvbox_material_transparent_background = true,
-				gruvbox_material_better_performance = true,
-			}) do
-				vim.g[key] = value
-			end
-
-			vim.api.nvim_create_autocmd("ColorScheme", {
-				group = vim.api.nvim_create_augroup("custom_highlights_gruvboxmaterial", {}),
-				pattern = "gruvbox-material",
-				callback = function()
-					local config = vim.fn["gruvbox_material#get_configuration"]()
-					local palette =
-						vim.fn["gruvbox_material#get_palette"](config.background, config.foreground, config.colors_override)
-
-					local set_hl = vim.fn["gruvbox_material#highlight"]
-
-					set_hl("NormalFloat", palette.none, palette.none)
-					set_hl("FloatBorder", palette.bg0, palette.none)
-					set_hl("TelescopeBorder", palette.bg0, palette.none)
-					set_hl("FloatTitle", palette.bg0, palette.none)
-				end,
-			})
-		end,
-	},
-	{
-		"olivercederborg/poimandres.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("poimandres").setup({
-				disable_background = true,
-				disable_float_background = true,
-				disable_italics = true,
-			})
-
-			vim.api.nvim_create_autocmd("ColorScheme", {
-				pattern = "poimandres",
-				callback = function() hl("WinSeparator", { link = "NonText" }) end,
-			})
-		end,
-	},
-	{
 		"rose-pine/neovim",
 		name = "rose-pine",
 		priority = 1000,
@@ -89,41 +39,6 @@ return {
 				TelescopeSelectionCaret = { bg = "Normal" },
 			},
 		},
-	},
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000,
-		config = function()
-			local bg = "#011628"
-			local bg_highlight = "#143652"
-			local bg_search = "#0A64AC"
-			local bg_visual = "#275378"
-			local fg = "#CBE0F0"
-			local fg_dark = "#B4D0E9"
-			local fg_gutter = "#627E97"
-			local border = "#547998"
-
-			require("tokyonight").setup({
-				transparent = true,
-				styles = { functions = {}, variables = {}, sidebars = "transparent", floats = "transparent" },
-				on_highlights = function(highlights, colors) highlights.TelescopeSelection = { bg = colors.none } end,
-
-				on_colors = function(colors)
-					colors.bg_statusline = colors.none
-
-					colors.bg = bg
-					colors.bg_highlight = bg_highlight
-					colors.bg_search = bg_search
-					colors.bg_visual = bg_visual
-					colors.border = border
-					colors.fg = fg
-					colors.fg_dark = fg_dark
-					colors.fg_float = fg
-					colors.fg_gutter = fg_gutter
-					colors.fg_sidebar = fg_dark
-				end,
-			})
-		end,
 	},
 	{
 		"bluz71/vim-nightfly-colors",
@@ -154,27 +69,5 @@ return {
 				end,
 			})
 		end,
-	},
-	{
-		"catppuccin/nvim",
-		priority = 1000,
-		name = "catppuccin",
-		opts = {
-			no_bold = true,
-			no_underline = true,
-			transparent_background = true,
-			term_colors = true,
-			integrations = {
-				grug_far = true,
-				nvim_surround = false,
-				mason = true,
-				noice = true,
-				-- telescope = {
-				-- 	enabled = true,
-				-- 	style = "nvchad",
-				-- },
-				which_key = true,
-			},
-		},
 	},
 }
