3173fd22b48f1ca9b1270877c12547d30d5027ee
Andrew Berty <andrewgeorgeberty@gmail.com>
2025-01-10 00:49:38 +0200

    nvim: colorschemes cleanup

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -2,25 +2,6 @@ local hl = function(name, opts) return vim.api.nvim_set_hl(0, name, opts) end
 
 return {
 	{
-		"tinted-theming/tinted-vim",
-		-- enabled = false,
-		config = function()
-			vim.g.tinted_background_transparent = 1
-			vim.g.tinted_colorspace = 256
-
-			vim.api.nvim_create_autocmd("ColorScheme", {
-				pattern = "base16-*",
-				callback = function()
-					hl("NormalFloat", { bg = "" })
-					hl("FloatBorder", { fg = "NvimDarkGray3" })
-					hl("FzfLuaBorder", { link = "FloatBorder" })
-					hl("TelescopeBorder", { link = "FloatBorder" })
-					hl("IblScope", { link = "Label" })
-				end,
-			})
-		end,
-	},
-	{
 		"rose-pine/neovim",
 		name = "rose-pine",
 		priority = 1000,
