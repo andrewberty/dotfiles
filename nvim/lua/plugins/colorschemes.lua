5e5f06e648771f26c9050fee2258249d91a13f29
Andrew Berty <andrewgeorgeberty@gmail.com>
2025-01-26 02:48:04 +0200

    nvim: vesper colorscheme; tmux: faded popoup border

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -2,6 +2,21 @@ local hl = function(name, opts) return vim.api.nvim_set_hl(0, name, opts) end
 
 return {
 	{
+		"datsfilipe/vesper.nvim",
+		config = function()
+			require("vesper").setup({
+				transparent = true,
+				italics = {},
+				overrides = {
+					FloatBorder = { link = "VertSplit" },
+					FzfLuaBorder = { link = "FloatBorder" },
+					NormalFloat = { bg = "none" },
+				},
+				palette_overrides = {},
+			})
+		end,
+	},
+	{
 		"rose-pine/neovim",
 		name = "rose-pine",
 		priority = 1000,
