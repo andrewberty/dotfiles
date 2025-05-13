59e9a503c6847a37ccea4a633e2d080ff29747fa
Andrew Berty <andrewgeorgeberty@gmail.com>
2025-05-13 21:41:16 +0300

    zed: add lazygit task

--- zed/install.sh
+++ zed/install.sh
@@ -5,9 +5,19 @@ target="$HOME/.config/zed"
 
 echo "Installing Zed"
 
+echo "linking settings.json"
 ln -s "$root/settings.json" "$target"
+
+echo "linking keymap.json"
 ln -s "$root/keymap.json" "$target"
+
+echo "linking tasks.json"
+ln -s "$root/tasks.json" "$target"
+
+echo "linking themes/"
 ln -s "$root/themes/" "$target"
+
+echo "linking snippets/"
 ln -s "$root/snippets/" "$target"
 
 echo "Installed Zed Successfully"
