e82a86ed8bd9e4a49f8a8021cab1faa2bef9783a
Andrew Berty <andrewgeorgeberty@gmail.com>
2025-05-18 19:51:52 +0300

    scripts: lua switcher to handle non-existing config keys

--- scripts/lua-switcher.lua
+++ scripts/lua-switcher.lua
@@ -1,25 +1,23 @@
 local M = {}
 
-function M.write_config(opts)
-	local firstInstance = opts.firstInstanceOnly or true
-	local file_name = opts.config_file or "ghostty/dynamic-config"
+M.key_exists = function(line, config_key)
+	local pattern = config_key .. " = "
+	return string.find(line, pattern, 1, true)
+end
 
+function M.write_config(opts)
+	local file_name = opts.config_file or "ghostty/dynamic-config"
 	local path = os.getenv("HOME") .. "/dotfiles/" .. file_name
 	local reload_script = os.getenv("HOME") .. "/dotfiles/scripts/reload-ghostty.scpt"
-	local promptAnswer
-	local argument = arg[1]
 
-	if not opts.config_key then
-		print("No config key provided")
-		return
-	end
+	local value = arg[1]
+	local lines = {}
 
 	if opts.prompt then
 		io.write(opts.prompt .. " -> ")
-		promptAnswer = io.read()
+		value = io.read()
 	end
 
-	local lines = {}
 	local file_read = io.open(path, "r")
 
 	if file_read then
@@ -31,30 +29,24 @@ function M.write_config(opts)
 		local line_found_and_replaced = false
 
 		for i, line in ipairs(lines) do
-			if string.find(line, opts.config_key, 1, true) then
+			if M.key_exists(line, opts.config_key) then
 				line_found_and_replaced = true
-				if opts.prompt then
-					lines[i] = opts.config_key .. " = " .. promptAnswer
-				else
-					lines[i] = opts.config_key .. " = " .. argument
-				end
-
-				-- replace the first occurrence only
-				if firstInstance then
-					break
-				end
+				lines[i] = opts.config_key .. " = " .. value
+				break
 			end
 		end
 
-		if line_found_and_replaced then
-			local file_write = io.open(path, "w")
+		local file_write = io.open(path, "w")
 
-			if file_write then
-				for _, line in ipairs(lines) do
-					file_write:write(line .. "\n")
-				end
-				file_write:close()
+		if file_write then
+			if not line_found_and_replaced then
+				table.insert(lines, opts.config_key .. " = " .. value)
 			end
+
+			for _, line in ipairs(lines) do
+				file_write:write(line .. "\n")
+			end
+			file_write:close()
 		end
 
 		os.execute("osascript " .. reload_script) -- reload ghostty
