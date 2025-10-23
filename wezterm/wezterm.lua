1b4ee0907f772f96594c98ba33071dc64c646fb5
Andrew Berty <andrewgeorgeberty@gmail.com>
2025-10-23 22:10:06 +0300

    wezterm: improve GPU performance by switching frontends and reducing some animations

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -19,22 +19,24 @@ config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
 config.window_close_confirmation = "NeverPrompt"
-config.macos_window_background_blur = 50
 config.window_background_opacity = G.opacity or 1.0
 config.window_decorations = "RESIZE | MACOS_FORCE_DISABLE_SHADOW"
 config.adjust_window_size_when_changing_font_size = false
 config.enable_scroll_bar = false
-config.front_end = "WebGpu"
 config.bidi_enabled = true
 config.bidi_direction = "LeftToRight"
-config.max_fps = 120
-config.default_cursor_style = "BlinkingBar"
+
+-- FRONTEND
+config.front_end = "WebGpu"
+config.max_fps = 30
+config.animation_fps = 1
+config.webgpu_power_preference = "LowPower"
 
 -- CURSOR
-config.cursor_blink_ease_in = "Linear"
-config.cursor_blink_ease_out = "Linear"
+config.default_cursor_style = "SteadyBar"
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
 config.hide_mouse_cursor_when_typing = true
-config.animation_fps = 120
 
 fonts.apply(config)
 colors.apply(config)
