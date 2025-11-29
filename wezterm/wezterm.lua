72bc2d043b8ef041999dd1b63b76c1ddeaa3addd
Andrew Berty <andrewgeorgeberty@gmail.com>
2025-11-29 10:02:37 +0200

    wezterm: cleanups

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,7 +14,6 @@ local config = wezterm.config_builder()
 config.set_environment_variables = { PATH = "/opt/homebrew/bin:" .. os.getenv("PATH") }
 -- config.default_prog = { "/bin/zsh", "-c", "~/dotfiles/scripts/tmux-attach.zsh" }
 
--- WINDOW
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
@@ -26,16 +25,8 @@ config.enable_scroll_bar = false
 config.bidi_enabled = true
 config.bidi_direction = "LeftToRight"
 
--- FRONTEND
 config.front_end = "WebGpu"
-config.max_fps = 30
-config.animation_fps = 1
-config.webgpu_power_preference = "LowPower"
-
--- CURSOR
 config.default_cursor_style = "SteadyBar"
-config.cursor_blink_ease_in = "Constant"
-config.cursor_blink_ease_out = "Constant"
 config.hide_mouse_cursor_when_typing = true
 
 fonts.apply(config)
