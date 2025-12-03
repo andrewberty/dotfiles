b615691be79801b5876a277cdcf368cfe058847b
Andrew Berty <andrewgeorgeberty@gmail.com>
2025-12-03 21:09:20 +0200

    nvim: configure render markdown

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,18 +1,38 @@
 return {
 	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		"MeanderingProgrammer/render-markdown.nvim",
 		keys = {
-			{
-				"<leader>p",
-				vim.cmd.MarkdownPreview,
-				noremap = true,
-				buffer = true,
-				desc = "[P]review Markdown",
+			{ "<leader>md", "<cmd>RenderMarkdown toggle<cr>", desc = "Toggle Render Markdown" },
+		},
+		opts = {
+			enabled = false,
+			heading = {
+				sign = false,
+				width = "block",
+				border = true,
+				above = "-",
+				below = "-",
+				left_pad = 2,
+				right_pad = 4,
+				backgrounds = {
+					"Normal",
+					"Normal",
+					"Normal",
+					"Normal",
+					"Normal",
+					"Normal",
+				},
+			},
+
+			code = {
+				sign = false,
+				language = true,
+				width = "block",
+				language_pad = 4,
+				left_pad = 4,
+				right_pad = 4,
+				inline_pad = 1,
 			},
 		},
-		ft = { "markdown" },
-		build = function() vim.fn["mkdp#util#install"]() end,
 	},
-	{ "MeanderingProgrammer/render-markdown.nvim", opts = {} },
 }
