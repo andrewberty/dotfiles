c4df99de9b07c425ac75638f3937bc89da7519ac
Andrew Berty <andrewgeorgeberty@gmail.com>
2025-12-05 19:37:03 +0200

    scripts: remove old zsh scripts

--- scripts/clear-global-bg.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-/opt/homebrew/bin/gawk -i inplace '
-  !/^background = /
-' ~/dotfiles/wezterm/globals.toml
--- scripts/fetch-builtin-colors.zsh
+++
@@ -1,1 +0,0 @@
-curl -s -H "Accept: application/json" https://raw.githubusercontent.com/wez/wezterm/refs/heads/main/docs/colorschemes/data.json | jq -r '.[].metadata.name' > ~/dotfiles/wezterm/data/colors.txt
