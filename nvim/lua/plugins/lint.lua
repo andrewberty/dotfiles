9177e07096dd764339c902eec5ef9df75bfba49b
Andrew Berty <andrewgeorgeberty@gmail.com>
2026-04-27 19:53:12 +0300

    Revert "nvim: add shellcheck lint"
    This reverts commit 906d2f248474f3e57655321e7ea35aafa7100989.

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -7,9 +7,6 @@ return {
 			typescript = { "eslint_d" },
 			javascriptreact = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
-			sh = { "shellcheck" },
-			bash = { "shellcheck" },
-			zsh = { "shellcheck" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
