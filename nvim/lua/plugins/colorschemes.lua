7408a3dae669625cc8bd8d8ea141d6d8d81589be
Andrew Berty <andrewgeorgeberty@gmail.com>
2026-05-06 20:53:26 +0300

    nvim: colors

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,6 +1,47 @@
 ---@diagnostic disable: missing-fields
 return {
 	{
+		"vague-theme/vague.nvim",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			require("vague").setup({
+				transparent = true,
+				bold = false,
+				italic = false,
+				on_highlights = function(hl, colors)
+					hl.FloatBorder = { fg = colors.comment }
+					hl.WinBar.bg = nil
+				end,
+				-- colors = {
+				-- 	bg = "#141415",
+				-- 	inactiveBg = "#1c1c24",
+				-- 	fg = "#cdcdcd",
+				-- 	floatBorder = "#878787",
+				-- 	line = "#252530",
+				-- 	comment = "#606079",
+				-- 	builtin = "#b4d4cf",
+				-- 	func = "#c48282",
+				-- 	string = "#e8b589",
+				-- 	number = "#e0a363",
+				-- 	property = "#c3c3d5",
+				-- 	constant = "#aeaed1",
+				-- 	parameter = "#bb9dbd",
+				-- 	visual = "#333738",
+				-- 	error = "#d8647e",
+				-- 	warning = "#f3be7c",
+				-- 	hint = "#7e98e8",
+				-- 	operator = "#90a0b5",
+				-- 	keyword = "#6e94b2",
+				-- 	type = "#9bb4bc",
+				-- 	search = "#405065",
+				-- 	plus = "#7fa563",
+				-- 	delta = "#f3be7c",
+				-- },
+			})
+		end,
+	},
+	{
 		"tinted-theming/tinted-nvim",
 		priority = 1000,
 		lazy = false,
